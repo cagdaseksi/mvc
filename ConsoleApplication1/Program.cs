772ca9b6239c07c4b8e8963cedf83ebc48829d76
cagdaseksioglu <cagdas_eksioglu@hotmail.com>
2015-03-30 11:08:00 +0300

    second

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,8 +11,6 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.ReadLine();
-            Console.ReadLine();
-            Console.ReadLine();
         }
     }
 }
